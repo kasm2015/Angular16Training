23ef16547fd3dd352cc635ea0cb82fc95b447efb
Syed Maqbul Hussain Kotwal <kasm20@gmail.com>
2024-03-22 08:21:30 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'SampleApplication';
+  title: string = 'SampleApplication';
+
+  myBtn: string = 'My Button';
+
+  //Attribute binding
+  isDisabled: boolean = false;
+
+  angularImage: string = '../assets/angular.png';
+  widthValue : string = "299";
+
+  //style binding
+  bgColor: string = "red";
+  titleColor : string = "white";
+  description: string = 'font-size:40px; color:blue';
+
+  //event binding
+  counter: number = 0;
+
+  increamentCounter() {
+    this.counter++;
+  }
+
+  decrementCounter() {
+    this.counter--;
+  }
+
+  //class binding
+  redText : boolean = true;
+
+  redTextWithValue : string = 'yes';
+
+  inputText : string = "sample value";
+
+  message: string = "This message is for ngClass directives";
+
+  classes: string = "danger text-size"
+
+  selectedColor : string = "red";
+
+  structuralDirectives: string = "Structure Directives";
+  isLoggedIn: boolean = false;
+  username: string = "Syed";
+
+  //Create an array
+  names: string[] = ['syed','rajesh','john','venkat','suhail','sujata'];
+
+  grade: string = "sgagd";
 }
